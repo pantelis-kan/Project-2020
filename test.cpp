ceedf9ac01c008323eb91c85aa1584ac955aecf1
TilemachosB <tilemaxosm@gmail.com>
2020-09-29 17:26:16 +0300

    version 29/10

--- test.cpp
+++ test.cpp
@@ -2,6 +2,15 @@ int main(int argc, char const *argv[])
 {
     //tesstttt
     cout << "in";
-    cout << "out";
+    cout << "outeeee";
+    int i;
+    i=1;
+    if (i==1)
+    {
+        /* code */
+    }
+    
+
+
     return 0;
 }
