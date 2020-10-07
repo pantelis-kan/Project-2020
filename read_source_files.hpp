f3bf036b720fcd897e2627658a63e65e665cb8bb
TilemachosB <tilemaxosm@gmail.com>
2020-10-07 11:20:51 +0300

    updated

---
+++ read_source_files.hpp
@@ -0,0 +1,20 @@
+#ifndef READ_SOURCE_FILES_HPP
+#define READ_SOURCE_FILES_HPP
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+#include "Point.hpp"
+
+using namespace std;
+
+int reverseInteger (int);
+
+int read_input_file(string &);
+
+void read_query_file();
+
+
+#endif
